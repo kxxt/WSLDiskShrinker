ee4b63e1cfe35c06709aa8680f77ee4a0451a566
iterma <110034155+iterma@users.noreply.github.com>
2022-07-27 21:35:57 +0200

    Exclude class if .NET5 or greater.

--- WSLDiskShrinker/Common/ProcessExtension.cs
+++ WSLDiskShrinker/Common/ProcessExtension.cs
@@ -1,28 +1,27 @@
-﻿using System.Threading;
+﻿#if !NET5_0_OR_GREATER
+
+using System.Threading;
 
 namespace WSLDiskShrinker.Common;
 
 // https://stackoverflow.com/questions/470256/process-waitforexit-asynchronously
 static class ProcessExtension
 {
-    /// <summary>
-    /// Waits asynchronously for the process to exit.
-    /// </summary>
+    /// <summary>Waits asynchronously for the process to exit.</summary>
     /// <param name="process">The process to wait for cancellation.</param>
-    /// <param name="cancellationToken">A cancellation token. If invoked, the task will return
-    /// immediately as canceled.</param>
+    /// <param name="cancellationToken">A cancellation token. If invoked, the task will return immediately as canceled.</param>
     /// <returns>A Task representing waiting for the process to end.</returns>
-    public static Task CustomWaitForExitAsync(this Process process,
-        CancellationToken cancellationToken = default)
+    public static Task CustomWaitForExitAsync(this Process process, CancellationToken cancellationToken = default)
     {
         if (process.HasExited) return Task.CompletedTask;
 
         var tcs = new TaskCompletionSource<object>();
         process.EnableRaisingEvents = true;
         process.Exited += (sender, args) => tcs.TrySetResult(process.ExitCode);
-        if (cancellationToken != default)
-            cancellationToken.Register(() => tcs.SetCanceled());
+        if (cancellationToken != default) cancellationToken.Register(() => tcs.SetCanceled());
 
         return process.HasExited ? Task.CompletedTask : tcs.Task;
     }
 }
+
+#endif
