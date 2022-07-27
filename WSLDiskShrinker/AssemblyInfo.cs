65f11063f57f7f01ca00523672079f1242b4a61f
kxxt <rsworktech@outlook.com>
2022-07-27 08:24:22 +0800

    fix: WPF already declared ssemly version elsewhere.

--- WSLDiskShrinker/AssemblyInfo.cs
+++ WSLDiskShrinker/AssemblyInfo.cs
@@ -1,5 +1,3 @@
-using System.Reflection;
-
 [assembly: ThemeInfo(
 	ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
 									 //(used if a resource is not found in the page,
@@ -8,6 +6,3 @@ using System.Reflection;
 											  //(used if a resource is not found in the page,
 											  // app, or any theme specific resource dictionaries)
 )]
-
-[assembly: AssemblyVersion("0.2.2")]
-[assembly: AssemblyFileVersion("0.2.2")]
